0a51c4962f3543b698ec549e4e79a1d3c60defb9
Attila SATAN <attila@lupuseum.com>
2015-08-26 01:08:32 +0300

    git init

---
+++ config/env/all.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+	app: {
+		title: 'Standart-4',
+		description: 'Standart-4',
+		keywords: 'Standart-4'
+	},
+	port: process.env.PORT || 3000,
+	templateEngine: 'nunjucks',
+	sessionSecret: 'MEAN',
+	sessionCollection: 'sessions',
+	assets: {
+		lib: {
+			css: [
+				'public/lib/bootstrap/dist/css/bootstrap.css',
+                'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+			],
+			js: [
+                'public/lib/jquery/dist/jquery.js',
+				'public/lib/bootstrap/dist/js/bootstrap.js'
+			]
+		},
+		css: [],
+		js: [],
+		tests: [
+			'public/lib/angular-mocks/angular-mocks.js',
+			'public/modules/*/tests/*.js'
+		]
+	}
+};
---
+++ config/filters.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = function(env){
+    
+    
+    
+    env.addFilter('json', function(object){
+        return JSON.stringify(object);
+    });
+    
+};
